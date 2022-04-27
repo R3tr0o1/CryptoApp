7d43078bb585fbae99b94f1713896fc8ff77e672
R3tr0o1 <rohan.budhathoki69@gmail.com>
2022-04-27 15:35:34 +0545

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:myapp/screens/SplashScreen/SplashScreen.dart';
+import 'package:myapp/screens/auth/login/authorizationScreen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(375, 812),
+      designSize: const Size(375, 812),
       builder: (_) {
-        return GetMaterialApp(
-          home: SplashScreen(),
+        return const GetMaterialApp(
+          home: AuthorizationScreen(),
         );
       },
     );
